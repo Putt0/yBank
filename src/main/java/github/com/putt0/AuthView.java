db81d1de7d40cc0a72b67186a1d8128e1841f08b
Reinaldy Oliveira <reinaldymc4@gmail.com>
2023-08-19 17:38:23 -0300

    Class AuthView; Responsável pela interface de login

---
+++ src/main/java/github/com/putt0/AuthView.java
@@ -0,0 +1,12 @@
+package github.com.putt0;
+
+import javax.swing.*;
+
+public class AuthView {
+
+    private JPanel jPanel;
+    private JTextField jTextField;
+    private JPasswordField jPasswordField;
+    private JButton jButton;
+    private JCheckBox jCheckBox;
+}
