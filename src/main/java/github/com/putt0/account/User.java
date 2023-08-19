76b8b02c65b6294d758510704072db0889189152
Reinaldy Oliveira <reinaldymc4@gmail.com>
2023-08-19 16:46:23 -0300

    Class User; Responsável por todos os dados de usuário

---
+++ src/main/java/github/com/putt0/account/User.java
@@ -0,0 +1,31 @@
+package github.com.putt0.account;
+
+public class User {
+
+    private String name, password;
+    private double cash;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getCash() {
+        return cash;
+    }
+
+    public void setCash(double cash) {
+        this.cash = cash;
+    }
+}
