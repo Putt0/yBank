d578c86ce82fdc2e9333f59c8aa1acaa2b71dbfb
Reinaldy Oliveira <reinaldymc4@gmail.com>
2023-08-19 17:47:46 -0300

    Update Main; Alterado o metodo de conexão SQL

--- src/main/java/github/com/putt0/Main.java
+++ src/main/java/github/com/putt0/Main.java
@@ -4,9 +4,7 @@ import github.com.putt0.storage.mysql.MySQL;
 
 public class Main {
 
-    private static final MySQL mySQL = new MySQL();
-
     public static void main(String[] args) {
-        mySQL.loadMySQL();
+        new MySQL().loadMySQL();
     }
 }
