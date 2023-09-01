914995049ac306ca7664a87706b262e4e3a510d4
Reinaldy Oliveira <reinaldymc4@gmail.com>
2023-09-01 13:36:45 -0300

    Define line 10 load Authview in Main

--- src/main/java/github/com/putt0/Main.java
+++ src/main/java/github/com/putt0/Main.java
@@ -1,10 +1,12 @@
 package github.com.putt0;
 
 import github.com.putt0.storage.mysql.MySQL;
+import github.com.putt0.view.Auth;
 
 public class Main {
 
     public static void main(String[] args) {
         new MySQL().loadMySQL();
+        new Auth().show();
     }
 }
